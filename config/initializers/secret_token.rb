becb32e33f80d5e3ddd568b9450fb7254935b470
Van Nguyen <soutennizavan@gmail.com>
2013-07-30 09:44:05 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo4mysql::Application.config.secret_key_base = '4bb89cf90cd2cb3b468c467843d27c0009afe0b203123c64156ae53e352b3f715d1e2ac0b379ce9f90c03a5fd7f463aed37591cb3551dc77acc4887f7848edae'
